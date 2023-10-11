a3f8f0999dca01169730a87cd29c759061bee1de
Enivaldo Queiroz <enivaldo.jecq@gmail.com>
2023-10-10 22:55:15 -0300

    Criando a estrutura do projeto, incluindo as referências e criando o contexto

---
+++ CleanArchMvc/CleanArchMvc.Infra.Data/Context/ApplicationDbContex.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Context
+{
+    public class ApplicationDbContex : DbContext
+    {
+        public ApplicationDbContex(DbContextOptions<ApplicationDbContex> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContex).Assembly);
+        }
+    }
+}
